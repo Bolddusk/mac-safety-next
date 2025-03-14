0c2bbc56e0f3e264c42f03a46358b3889387b9b2
smart fix <smartfix@smarts-MacBook-Pro.local>
2025-03-15 02:19:25 +0500

    remove shadow effect and add tags - 7

--- src/component/home/AnimateHeroTitle.tsx
+++ src/component/home/AnimateHeroTitle.tsx
@@ -4,11 +4,11 @@ const AnimateHeroTitle = () => {
   return (
     <div className="max-w-3xl mx-auto">
       <div className="text-4xl sm:text-5xl md:text-6xl font-bold space-y-8 md:space-y-12">
-        <h1 className="text-white">Intelligence at Every Level</h1>
+        <span className="text-white">Intelligence at Every Level</span>
         <AnimateTitle />
-        <h1 className="text-2xl sm:text-3xl md:text-4xl text-white/90 mt-4">
+        <span className="text-2xl sm:text-3xl md:text-4xl text-white/90 mt-4">
           Working as One
-        </h1>
+        </span>
       </div>
     </div>
   );
