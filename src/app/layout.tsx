b5b5bd5cb3f2eb11d51b9401c077bf5e61cc3bcd
smart fix <smartfix@smarts-MacBook-Pro.local>
2025-03-16 04:56:45 +0500

    add cononicals tags + fix page url in sitemap - 6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Script from "next/script";
 import { jsonLd } from "@/utils/schema";
+import { Geist, Geist_Mono } from "next/font/google";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   icons: "/uploads/FaviconMAC.png",
@@ -48,7 +49,7 @@ export default function RootLayout({
         </Script>
       </head>
       <body
-      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
