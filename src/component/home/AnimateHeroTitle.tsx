d8d19500008c2936ccfcc079fb5c088676331f7a
smart fix <smartfix@smarts-MacBook-Pro.local>
2025-03-16 04:30:41 +0500

    add cononicals tags + fix page url in sitemap - 3

--- src/component/home/AnimateHeroTitle.tsx
+++ src/component/home/AnimateHeroTitle.tsx
@@ -3,7 +3,7 @@ import AnimateTitle from "./AnimateTitle";
 const AnimateHeroTitle = () => {
   return (
     <div className="max-w-3xl mx-auto">
-      <div className="p-4 md:p-6 text-4xl sm:text-5xl md:text-6xl font-bold">
+      <div className="p-4 md:p-6 text-4xl md:text-6xl font-bold">
         <span className="text-white">Intelligence at Every Level</span>
         <AnimateTitle />
         <span className="text-2xl sm:text-3xl md:text-4xl text-white/90 mt-4">
